5f257545661ef88597fd5380beec924e182364e7
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-04-18 01:10:43 +0300

    upload FE with login and register component + app context and reducers commented out until connection with BE. CSS is pending... of course lol

--- server/cilent/src/App.js
+++ server/cilent/src/App.js
@@ -1,70 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
-import Start from './components/Start';
-import Question from './components/Question';
-import End from './components/End';
-import Modal from './components/Modal';
-import quizData from './data/quiz.json';
-
-let interval;
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Game from "./pages/Game";
+import Register from "./pages/Register";
+import ProtectedRoute from "./ProtectedRoute";
 
 const App = () => {
-  const [step, setStep] = useState(1);
-  const [activeQuestion, setActiveQuestion] = useState(0);
-  const [answers, setAnswers] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    if(step === 3) {
-      clearInterval(interval);
-    }
-  }, [step]);
-
-  const quizStartHandler = () => {
-    setStep(2);
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
-    }, 1000);
-  }
-
-  const resetClickHandler = () => {
-    setActiveQuestion(0);
-    setAnswers([]);
-    setStep(2);
-    setTime(0);
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 1);
-    }, 1000);
-  }
-
   return (
-    <div className="App">
-      {step === 1 && <Start onQuizStart={quizStartHandler} />}
-      {step === 2 && <Question 
-        data={quizData.data[activeQuestion]}
-        onAnswerUpdate={setAnswers}
-        numberOfQuestions={quizData.data.length}
-        activeQuestion={activeQuestion}
-        onSetActiveQuestion={setActiveQuestion}
-        onSetStep={setStep}
-      />}
-      {step === 3 && <End 
-        results={answers}
-        data={quizData.data}
-        onReset={resetClickHandler}
-        onAnswersCheck={() => setShowModal(true)}
-        time={time}
-      />}
-
-      {showModal && <Modal 
-        onClose={() => setShowModal(false)}
-        results={answers}
-        data={quizData.data}
-      />}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            // <ProtectedRoute>
+            <Game />
+            // </ProtectedRoute>
+          }
+        ></Route>
+        <Route path="/landing" element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
