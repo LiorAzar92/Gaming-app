087d4f2a9f6a3d374a45e3957f68e94c0d9dc024
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-04-23 03:02:11 +0300

    improve function to get results from db because I have insomnia

--- server/client/src/components/Start.js
+++ server/client/src/components/Start.js
@@ -13,49 +13,50 @@ const Start = ({ onQuizStart }) => {
     const nickname = user?.nickname;
     const lastScoreObtained = await axios.get(`${URL}lastScore/${nickname}`);
     if (lastScoreObtained.data.latestScore == null) {
-      setLastScore("not available");
+      setLastScore(null);
     } else {
       setLastScore(lastScoreObtained.data.latestScore.score);
     }
     const highScoreObtained = await axios.get(`${URL}highScore/${nickname}`);
     if (highScoreObtained.data.latestScore == null) {
-      setHighScore("not available");
+      setHighScore(null);
     } else {
       setHighScore(highScoreObtained.data.latestScore.score);
     }
-
-    // console.log(
-    //   "LAST SCORE:",
-    //   lastScoreObtained.data.latestScore.score,
-    //   "HIGHEST SCORE:",
-    //   highScoreObtained.data.latestScore.score
-    // );
   }
   useEffect(() => {
     getScores();
   }, [getScores]);
+
+  function postScores() {
+    if (lastScore && lastScore == highScore) {
+      return <p>Your last -and highest- score is {lastScore}</p>;
+    }
+    if (lastScore && lastScore != highScore) {
+      return (
+        <div>
+          <p>
+            <strong>Last score: </strong>
+            {lastScore}
+          </p>
+          <p>
+            <strong>Highest score: </strong>
+            {highScore}
+          </p>
+        </div>
+      );
+    }
+    if (lastScore == null) {
+      return <p>Seems you haven't played yet!</p>;
+    }
+  }
 
   return (
     <div className="card">
       <div className="card-content">
         <div className="content">
           <h1> Challenge yourself, {nickname}!</h1>
-          {!lastScore ? (
-            ""
-          ) : (
-            <p>
-              <strong>Last score: </strong>
-              {lastScore}
-            </p>
-          )}
-          {highScore ? (
-            <p>
-              <strong>Highest score: </strong>
-              {highScore}
-            </p>
-          ) : (
-            ""
-          )}
+          {postScores()}
           <button className="button is-info is-medium" onClick={onQuizStart}>
             Start
           </button>
